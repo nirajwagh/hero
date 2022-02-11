7031a8352aa391dd2323693e4acea56fba9c3fa5
Niraj Wagh <iamnirajwagh@gmail.com>
2022-02-11 22:59:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const app = require("express")();
+const { sleep } = require('sleep');
 
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
+  sleep(5);
   res.send("Hello world");
 });
 
